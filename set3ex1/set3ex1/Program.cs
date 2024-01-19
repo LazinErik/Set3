8380d1beae991dd1956ee36a2f6a53f593724e0c
Erik Lazin <154979602+LazinErik@users.noreply.github.com>
2024-01-20 00:07:57 +0200

    Add files via upload

---
+++ set3ex1/set3ex1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace set3ex1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, suma = 0;
+            Console.Write("n = ");
+            n= int.Parse(Console.ReadLine());
+            int[] v = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                v[i] = int.Parse(Console.ReadLine());
+                suma = suma + v[i];
+            }
+            Console.WriteLine($"Suma elementelor vectorului este: {suma}");
+        }
+    }
+}
---
+++ set3ex2/set3ex2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace set3ex2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, k, gasit=0;
+            Console.Write("n = ");
+            n = int.Parse(Console.ReadLine());
+            Console.Write("k = ");
+            k = int.Parse(Console.ReadLine());
+            int[] v = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                v[i] = int.Parse(Console.ReadLine());
+                if (v[i] == k)
+                {
+                    Console.WriteLine($"Numarul {k} apare pe pozitia {i} in vector");
+                    gasit = 1;
+                    break;
+                }
+            }
+            if (gasit == 0) Console.WriteLine("-1");
+        }
+    }
+}
